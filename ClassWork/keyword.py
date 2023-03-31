7a5056d002bbaa91a296e78a87a67712b0f0a851
Ektapatel318 <ektapatel0831@gmail.com>
2023-03-31 22:05:54 +0530

    Create keyword.py
    keyword

---
+++ ClassWork/keyword.py
@@ -0,0 +1,18 @@
+"""
+continue : it is use to skip the condition
+break : it is use to stop the loop
+
+
+
+for i in range(1,10):
+    if i==4 or i==7:
+        continue
+    else:
+        print("I : ",i)
+"""
+
+for i in range(1,10):
+    if i%2==0:
+        continue
+    else:
+        print(i)
