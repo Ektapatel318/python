b59705721bd4e11300494db7b54ee035bbb9e2fc
Ektapatel318 <ektapatel0831@gmail.com>
2023-04-14 09:17:32 +0530

    Update Module 3__Python _Collections, functions and Modules in Python Collections, functions and Modules in Python.py
    upadate

--- Assignment/Module 3__Python _Collections, functions and Modules in Python Collections, functions and Modules in Python.py
+++ Assignment/Module 3__Python _Collections, functions and Modules in Python Collections, functions and Modules in Python.py
@@ -730,7 +730,7 @@ min_number = min(decimal_numbers)
 print("The maximum number is:", max_number)
 print("The minimum number is:", min_number)
 
-""" 
+
 
 # Mini project : 
 
@@ -740,29 +740,24 @@ print("The minimum number is:", min_number)
 # letters to generate the password. Also, keep a check that password length is more than 8 characters. 
 # Note: Include Exception handling wherever required. Also, make a ‘User’ class and store the details like user 
 # id, name and password of each user as a tuple.
-import exception handling 
+ 
 import string
 import random
+
 s = []
 s1 = string.ascii_letters
 s2 = string.ascii_lowercase
 s3 = string.ascii_uppercase
 s4 = string.punctuation
-try:
-    len = int(input("enter password length\n"))
-
+len = int(input("enter password length\n"))
+      
 s.extend(s1)
 s.extend(s2)
 s.extend(s3)
 s.extend(s4)
-
-
 print("".join(random.sample(s,len)))
 
-except:
-    print("enter the valid input ")
-
-
+"""
 
 
 
